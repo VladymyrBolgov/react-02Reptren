347e36d0c77d647a5c1d852a321cce6cd8b2a5bf
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-09-13 15:06:23 +0300

    п

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-// import Counter from './components/Counter';
+import Counter from './components/Counter';
 // import Dropdown from './components/Dropdown';
 // import ColorPicker from './components/ColorPicker';
-import TodoList from './components/TodoList';
-import initialTodos from './todos.json';
+//import TodoList from './components/TodoList';
+//import initialTodos from './todos.json';
 
-// const colorPickerOptions = [
-//   { label: 'red', color: '#F44336' },
-//   { label: 'green', color: '#4CAF50' },
-//   { label: 'blue', color: '#2196F3' },
-//   { label: 'grey', color: '#607D8B' },
-//   { label: 'pink', color: '#E91E63' },
-//   { label: 'indigo', color: '#3F51B5' },
-// ];
+ const colorPickerOptions = [
+   { label: 'red', color: '#F44336' },
+   { label: 'green', color: '#4CAF50' },
+   { label: 'blue', color: '#2196F3' },
+   { label: 'grey', color: '#607D8B' },
+   { label: 'pink', color: '#E91E63' },
+   { label: 'indigo', color: '#3F51B5' },
+ ];
 
+const App = () => (
+  <>
+    <h1>Состояние компонента</h1>
+    <Counter />
+  </>
+)
+
+/*
 class App extends Component {
   state = {
     todos: initialTodos,
@@ -34,23 +42,23 @@ class App extends Component {
       0,
     );
 
-    return (
-      <>
-        <h1>Состояние компонента</h1>
+   // return (
+    //  <>
+    //    <h1>Состояние компонента</h1>
 
-        {/* <Counter initialValue={10} /> */}
-        {/* <Dropdown /> */}
-        {/* <ColorPicker options={colorPickerOptions} /> */}
+     //   {/* <Counter initialValue={10} /> *//*}*/
+     //   {/* <Dropdown /> */}
+     //   {/* <ColorPicker options={colorPickerOptions} /> */}
 
-        <div>
-          <p>Общее кол-во: {totalTodoCount}</p>
-          <p>Кол-во выполненных: {completedTodoCount}</p>
-        </div>
+     //   <div>
+     //     <p>Общее кол-во: {totalTodoCount}</p>
+      //    <p>Кол-во выполненных: {completedTodoCount}</p>
+     //   </div>
 
-        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
-      </>
-    );
-  }
-}
+     //   <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+   //   </>
+ //   );
+//  }
+//}
 
 export default App;
--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -3,44 +3,31 @@ import Controls from './Controls';
 import Value from './Value';
 import './Counter.css';
 
-class Counter extends Component {
-  static defaultProps = {
-    initialValue: 0,
-  };
-
-  static propTypes = {
-    //
-  };
-
-  state = {
-    value: this.props.initialValue,
-  };
-
-  handleIncrement = () => {
-    this.setState(prevState => ({
-      value: prevState.value + 1,
-    }));
-  };
-
-  handleDecrement = () => {
-    this.setState(prevState => ({
-      value: prevState.value - 1,
-    }));
-  };
-
+class Counter extends React.Component {
   render() {
-    const { value } = this.state;
-
     return (
-      <div className="Counter">
-        <Value value={value} />
-
-        <Controls
-          onIncrement={this.handleIncrement}
-          onDecrement={this.handleDecrement}
-        />
+      <div className='Counter'>
+      <span className='Counter__value'>0</span>
+  
+      <div className='Counter__controls'>
+          <button
+            type='button'
+            onClick={() => {
+              console.log('Кликнули в увеличить')
+            }}
+          >
+            Увеличить на 1
+          </button>
+          <button
+            type='button'
+            onClick={() => {
+              console.log('Кликнули на уменьшение')
+            }}
+          >
+            Уменьшить на 1</button>
       </div>
-    );
+    </div>
+    )
   }
 }
 
