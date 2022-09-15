15ef04ad33f4d03073bc1750350be9ac343eaa9d
VladymyrBolgov <vladymyr.bolgov@gmail.com>
2022-09-15 23:28:31 +0300

    д

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import React, { Component } from 'react';
-import ColorPicker from './components/ColorPicker/ColorPicker';
-import Counter from './components/Counter';
- import Dropdown from './components/Dropdown';
- //import ColorPicker from './components/ColorPicker';
-//import TodoList from './components/TodoList';
-//import initialTodos from './todos.json';
+//import ColorPicker from './components/ColorPicker'
+//import Counter from './components/Counter';
+//import Dropdown from './components/Dropdown';
+import TodoList from './components/TodoList';
+import initialTodos from './todos.json';
 
- const colorPickerOptions = [
-   { label: 'red', color: '#F44336' },
-   { label: 'green', color: '#4CAF50' },
-   { label: 'blue', color: '#2196F3' },
-   { label: 'grey', color: '#607D8B' },
-   { label: 'pink', color: '#E91E63' },
-   { label: 'indigo', color: '#3F51B5' },
- ];
+ //const colorPickerOptions = [
+ //  { label: 'red', color: '#F44336' },
+ //  { label: 'green', color: '#4CAF50' },
+ //  { label: 'blue', color: '#2196F3' },
+ //  { label: 'grey', color: '#607D8B' },
+ //  { label: 'pink', color: '#E91E63' },
+ //  { label: 'indigo', color: '#3F51B5' },
+ //];
 
-const App = () => (
-  <>
-    <h1>Состояние компонента</h1>
-    {/**/}
-    <Counter initialValue={10} />
-    <Dropdown />
-    <ColorPicker options={colorPickerOptions} />
-  </>
-)
+//const App = () => (
+// <>
+//   <h1>Состояние компонента</h1>
+//   {/**/}
+//   <Counter initialValue={10} />
+//    <Dropdown />
+//   <ColorPicker options={colorPickerOptions} />
+// </>
+//)
 
-/*
 class App extends Component {
   state = {
     todos: initialTodos,
@@ -34,8 +32,8 @@ class App extends Component {
   deleteTodo = todoId => {
     this.setState(prevState => ({
       todos: prevState.todos.filter(todo => todo.id !== todoId),
-    }));
-  };
+    }))
+  }
 
   render() {
     const { todos } = this.state;
@@ -44,25 +42,23 @@ class App extends Component {
     const completedTodoCount = todos.reduce(
       (total, todo) => (todo.completed ? total + 1 : total),
       0,
-    );
+    )
+   
+    return (
+      <>
+        <h1>Состояние компонента</h1>
+        <div>
+          <p>Общее кол-во туду: {totalTodoCount}</p>
+          <p>Кл-во выполненных: {completedTodoCount}</p>
+        </div>
 
-   // return (
-    //  <>
-    //    <h1>Состояние компонента</h1>
-
-     //   {/* <Counter initialValue={10} /> *//*}*/
-     //   {/* <Dropdown /> */}
-     //   {/* <ColorPicker options={colorPickerOptions} /> */}
-
-     //   <div>
-     //     <p>Общее кол-во: {totalTodoCount}</p>
-      //    <p>Кол-во выполненных: {completedTodoCount}</p>
-     //   </div>
-
-     //   <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
-   //   </>
- //   );
-//  }
-//}
+        <TodoList todos={todos} onDeleteTodo={this.deleteTodo} />
+    </>
+    )
+  }
+}
 
 export default App;
+
+
+
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './TodoList.css';
 
 const TodoList = ({ todos, onDeleteTodo }) => (
-  <ul className="TodoList">
-    {todos.map(({ id, text }) => (
-      <li key={id} className="TodoList__item">
-        <p className="TodoList__text">{text}</p>
+  <ul className='TodoList'>
+    {todos.map(({id, text}) => (
+      <li key={id} className='TodoList__item'>
+        <p className='TodoList__text'>{text}</p>
         <button onClick={() => onDeleteTodo(id)}>Удалить</button>
-      </li>
+    </li>
     ))}
   </ul>
-);
+)
 
 export default TodoList;
